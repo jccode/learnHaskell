f16be8bc24435e2a857f2317313882627a955de5
jcchen <junchangchen@gmail.com>
2013-05-07 01:15:42 +0800

    ...

--- LearnYouAHaskell/b.hs
+++ LearnYouAHaskell/b.hs
@@ -45,7 +45,6 @@ fromList' = foldr (\(k, v) acc -> Map.insert k v acc) Map.empty
 
 
 
-
 ------------------------------------
 ------------------------------------
 
@@ -66,3 +65,41 @@ data Maybe' a = Nothing' | Just' a deriving (Show)
 
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
 		   deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+
+type PhoneBook = [(Name, PhoneNumber)]
+type Name = String
+type PhoneNumber = String
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+--inPhoneBook name number book = if ret == Nothing then False else  ret == Just number
+--	where ret = Map.lookup name $ Map.fromList book
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
+
+
+
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map = 
+	case Map.lookup lockerNumber map of 
+		Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+		--Just (state, code) -> if state /= Taken 
+		--						then Right code 
+		--						else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+		Just (state, code) -> case state of 
+									Taken -> Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+									Free -> Right code 
+
+lockers :: LockerMap
+lockers = Map.fromList [
+	(100, (Taken, "ZD39I")), 
+	(101, (Free, "JAH3I")), 
+	(102, (Free, "IQWA0")), 
+	(103, (Free, "IEYI9")), 
+	(105, (Taken, "9BDEL")), 
+	(109, (Taken, "87HFK")), 
+	(110, (Taken, "ZD3MN"))
+	]
+
+
