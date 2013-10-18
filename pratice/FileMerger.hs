598df1d93634154a39d16abdcbd749dbd86955ff
jcchen <junchangchen@gmail.com>
2013-10-18 19:38:23 +0800

    FileMerger.hs update

--- pratice/FileMerger.hs
+++ pratice/FileMerger.hs
@@ -1,7 +1,21 @@
 
 import System.Directory
+import Data.List
+
 
 main = do
-  putStrLn "main"
-  let files = [x | x <- getDirectoryContents ]
-  mapM_ putStrLn files
+  putStrLn "Hello FileMerger beginning..."
+  let dir = "d:/HGST/MFG/processing/HDD_WEBSPC_CR/C137/log/"
+      destFile = "./a.txt"
+  files <- getDirectoryContents dir
+  let filesToBeProcess = [x | x <- files, isInfixOf ".log" x]
+  mapM_ putStrLn filesToBeProcess
+  -- mergeToOneFile destFile filesToBeProcess
+  mergeToOneFile destFile files
+
+
+mergeToOneFile :: FilePath -> [FilePath] -> IO ()
+mergeToOneFile file inputFilePath = do
+  putStrLn "merge to file"
+
+
