0fbda3b7059a06cd57b020f69bfc6aa6fcd4cfab
jcchen <junchangchen@gmail.com>
2013-10-31 10:05:25 +0800

    delete monad.hs, rename monad2.hs to monad.hs

--- LearnYouAHaskell/monad.hs
+++ LearnYouAHaskell/monad.hs
@@ -1,7 +1,43 @@
+
 import Control.Monad.Writer
+
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  return (a*b)
+
+
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Monoid (DiffList a) where
+  mempty = DiffList (\xs -> [] ++ xs)
+  (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+  tell (toDiffList ["0"])
+finalCountDown x = do
+  finalCountDown (x-1)
+  tell (toDiffList [show x])
+
+finalCountDown' :: Int -> Writer [String] ()
+finalCountDown' 0 = do
+  tell ["0"]
+finalCountDown' x = do
+  finalCountDown' (x-1)
+  tell [show x]
+
 
 lookFold :: Int -> Int -> Writer [String] Int
 lookFold acc x = do
   tell ["currently x = " ++ (show x) ++ ", and acc = " ++ (show acc)]
   return (acc + x)
-  
